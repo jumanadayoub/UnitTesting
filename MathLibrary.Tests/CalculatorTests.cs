30d187e90a6d0362f1f89678381927cd40e80ed7
jumanadayoub <jumanad500@gmail.com>
2025-06-14 02:10:15 +0200

    first commit

---
+++ MathLibrary.Tests/CalculatorTests.cs
@@ -0,0 +1,10 @@
+﻿namespace MathLibrary.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ MathLibrary/Calculator.cs
@@ -0,0 +1,6 @@
+﻿namespace MathLibrary;
+
+public class Class1
+{
+
+}
